1b7cd4398b833fa1a6b32092836aadd695ec0185
Andrey Baryshev <andrey.bar66@gmail.com>
2024-08-22 20:23:36 +0500

    Update - Start

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Study-lib
 
+A repository with its own implementation of various data structures or already existing libraries
--- Vector/Vector.h
+++ Vector/Vector.h
@@ -1,96 +1,22 @@
-#pragma once
+#include "Node.h"
 
-template <typename Type>
-struct Node {
-    Type value;
-    Node* nextNode;
-    Node(Type value): value(value), nextNode(nullptr) {};
-};
-
-template <typename Type>
 class Vector {
 private:
-    typename Node<Type>::Node* headNode;
-    typename Node<Type>::Node* tailNode;
-    unsigned long long length = 0;
+    Node* headNode;
+    Node* tailNode;
+    unsigned long long size;
 
 public:
-    // ...
-    Vector<Type>(): headNode(nullptr), tailNode(nullptr), length(0) {};
-
-    // ...
-    void push(Type value) {
-        typename Node<Type>::Node* ptr = new Node(value);
-
-        // ...
-        if (!headNode) {
-            headNode = ptr;
-            tailNode = ptr;
-            length++;
-            return;
-        }
-
-        // ...
-        tailNode->nextNode = ptr;
-        tailNode = ptr;
-        length++;
-    }
-
-    // ...
+    #pragma region size()
+    /// <summary> Get the length of the vector </summary>
+    /// <returns> Unsigned long long value </returns>
     unsigned long long size() {
-        // ...
-        return length;
+        return size;
     }
-
-    // ...
-    Type pop() {
-        // ...
-        if (!length) {
-            throw ERROR_GET_INDEX_ARRAY;
-        }
-
-        // ...
-        Type value = tailNode->value;
-        typename Node<Type>::Node* ptr = headNode;
-
-        // ...
-        while(ptr->nextNode != tailNode) {
-            ptr = ptr->nextNode;
-        }
-        // ...
-        tailNode = ptr;
-
-        // ...
-        delete ptr->nextNode->value;
-        delete ptr->nextNode->nextNode;
-
-        // ...
-        return value;
-    }
-
-    // ...
-    Type peek() {
-        // ...
-        return tailNode.value;
-    }
+    #pragma endregion
 
 
-    // ...
-    Type operator[](unsigned long long indexGet) {
-        unsigned long long indexCurrent = 0;
+    void push(type value, bool sort = false) {
 
-        // ...
-        if (length <= indexGet || indexGet < 0) {
-            throw ERROR_GET_INDEX_ARRAY;
-        }
-
-        // ...
-        typename Node<Type>::Node* ptr = headNode;
-        while(indexCurrent < indexGet) {
-            ptr = ptr->nextNode;
-        }
-
-        // ...
-        return ptr->value;
     }
 };
